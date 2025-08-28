b9910abc810500d5b8b27238805cfe6b70b352e0
nandajfa <nanda.jfa@gmail.com>
2025-08-28 19:29:11 -0300

    feat: create save-image hook

---
+++ src/hooks/save-image.ts
@@ -0,0 +1,24 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/hook.html
+import type { HookContext } from '../declarations'
+
+export const saveImage = async (context: HookContext) => {
+  console.log(`Running hook save-image on ${context.path}.${context.method}`)
+  console.log('data', context.result.id)
+
+  try {
+    const profileService = context.app.service("profile");
+
+    const userProfile = await profileService.find({
+		query: { user_id: context.params.user.id }
+	  });
+	const currentProfile = userProfile.data[0];
+
+    await profileService.patch(currentProfile.id, {
+        photo: context.result.id
+    },
+	context.params);
+
+  } catch (error) {
+    console.error('Error updating profile:', error);
+  }
+}
