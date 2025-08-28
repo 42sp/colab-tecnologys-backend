e0261a44df8d797af986654fd5d6f8a268319db7
João Pedro <jpedro-lima@outlook.com>
2025-08-28 10:30:39 -0300

    chore: removed all services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,6 @@
-import { access } from './access/access'
-import { jobs } from './jobs/jobs'
-import { tasks } from './tasks/tasks'
-import { user } from './users/users'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
-	app.configure(access)
-	app.configure(jobs)
-	app.configure(tasks)
-	app.configure(user)
 	// All services will be registered here
 }
