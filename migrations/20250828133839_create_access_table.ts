3fb7574884ab2fe51d7ffeb5df13e759939f74ac
João Pedro <jpedro-lima@outlook.com>
2025-08-28 10:45:12 -0300

    feat: add migrations to create access and profile table

---
+++ migrations/20250828133839_create_access_table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('access', (table) => {
+		table.uuid('id').primary().unique()
+		table.string('email', 255).notNullable().unique()
+		table.string('password').notNullable()
+		table.timestamp('created_at').defaultTo(knex.fn.now())
+		table.timestamp('updated_at').defaultTo(knex.fn.now())
+	})
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('access')
+}
---
+++ migrations/20250828134137_create_profile_table.ts
@@ -0,0 +1,35 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('profile', (table) => {
+		table.uuid('id').primary().unique()
+
+		table.string('name', 100)
+		table.date('date_of_birth')
+		table.string('cpf', 11).unique()
+		table.string('registration_code', 50).unique()
+		table.string('phone', 20)
+		table.text('photo')
+
+		table.string('address', 255)
+		table.string('city', 100)
+		table.string('state', 2)
+		table.string('postcode', 15)
+
+		table.uuid('access_id').notNullable().unique()
+		table.foreign('access_id').references('id').inTable('access')
+
+		table.uuid('role_id')
+		table.foreign('role_id').references('id').inTable('roles')
+
+		table.boolean('is_active').defaultTo(true)
+		table.boolean('is_available').defaultTo(true)
+
+		table.timestamp('created_at').defaultTo(knex.fn.now())
+		table.timestamp('updated_at').defaultTo(knex.fn.now())
+	})
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('profile')
+}
