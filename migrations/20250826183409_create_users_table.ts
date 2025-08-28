0149b0cd42986089ccbf028a8357f33a20f8c614
João Pedro <jpedro-lima@outlook.com>
2025-08-28 10:31:12 -0300

    chore: created migrations users, roles, and populate roles table

---
+++ migrations/20250826183409_create_users_table.ts
@@ -0,0 +1,35 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('users', (table) => {
+		table.uuid('id').primary()
+
+		table.string('name', 100)
+		table.date('date_of_birth')
+		table.string('cpf', 11).unique().notNullable()
+		table.string('registration_code', 50).unique()
+		table.string('phone', 20)
+		table.text('photo')
+
+		table.string('email', 255).unique().notNullable()
+		table.text('password_hash').notNullable()
+
+		table.uuid('role_id').notNullable()
+		table.foreign('role_id').references('id').inTable('roles')
+
+		table.string('address', 255)
+		table.string('city', 100)
+		table.string('state', 2)
+		table.string('postcode', 15)
+
+		table.boolean('is_active').defaultTo(true)
+		table.boolean('is_available').defaultTo(true)
+
+		table.timestamp('created_at').defaultTo(knex.fn.now())
+		table.timestamp('updated_at').defaultTo(knex.fn.now())
+	})
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('users')
+}
