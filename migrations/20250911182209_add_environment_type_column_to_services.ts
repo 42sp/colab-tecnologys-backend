d5f33aa2e2c6b983caa60cf958b560b3caabed41
nandajfa <nanda.jfa@gmail.com>
2025-09-11 15:28:24 -0300

    refactor: add new column environment_type  migration and update services

---
+++ migrations/20250911182209_add_environment_type_column_to_services.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.table('services', (table) => {
+		table.string('environment_type', 2)
+	})
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.table('services', (table) => {
+		table.dropColumn('environment_type')
+	})
+}
+
