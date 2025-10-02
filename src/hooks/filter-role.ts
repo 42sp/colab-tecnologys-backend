0454b7f31b2caa073a67c69b604066c9ebd27a12
Phil <dev@lipe.ph>
2025-10-02 20:22:35 -0300

    chore: remove logs

--- src/hooks/filter-role.ts
+++ src/hooks/filter-role.ts
@@ -3,7 +3,7 @@ import type { HookContext } from '../declarations'
 
 export const filterRole = async (context: HookContext) => {
   console.log(`Running hook filter-role on ${context.path}.${context.method}`)
-  console.log('data', context.params)
+  //console.log('data', context.params)
   const role_id = (await context.app.service('profile').get(context.params.user.id)).role_id
   const level = (await context.app.service('roles').get(role_id)).hierarchy_level
 
