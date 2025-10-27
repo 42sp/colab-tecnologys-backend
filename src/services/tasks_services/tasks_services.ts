801708996b8d865318101f2bba9ab6ad3ce36b96
Phil <dev@lipe.ph>
2025-10-27 16:29:21 -0300

    feat: add two more services to swagger, task services and report

--- src/services/tasks_services/tasks_services.ts
+++ src/services/tasks_services/tasks_services.ts
@@ -1,4 +1,119 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
+/**
+ * @openapi
+ * /tasks_services:
+ *   get:
+ *     tags:
+ *       - Tasks Services
+ *     summary: Listar serviços de tarefas
+ *     description: Retorna lista de serviços associados a tarefas
+ *     responses:
+ *       200:
+ *         description: Lista de serviços de tarefas retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 total:
+ *                   type: number
+ *                 limit:
+ *                   type: number
+ *                 skip:
+ *                   type: number
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     description: Objeto dinâmico com propriedades variáveis
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *   post:
+ *     tags:
+ *       - Tasks Services
+ *     summary: Criar novo serviço de tarefa
+ *     description: Cria uma nova associação entre tarefa e serviço
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             description: Objeto com propriedades dinâmicas
+ *             example:
+ *               task_id: "uuid-da-tarefa"
+ *               service_id: "uuid-do-servico"
+ *     responses:
+ *       201:
+ *         description: Serviço de tarefa criado com sucesso
+ *       400:
+ *         $ref: '#/components/responses/BadRequestError'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *
+ * /tasks_services/{id}:
+ *   get:
+ *     tags:
+ *       - Tasks Services
+ *     summary: Buscar serviço de tarefa por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       200:
+ *         description: Serviço de tarefa encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       404:
+ *         $ref: '#/components/responses/NotFoundError'
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *   patch:
+ *     tags:
+ *       - Tasks Services
+ *     summary: Atualizar serviço de tarefa
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             description: Propriedades a serem atualizadas
+ *     responses:
+ *       200:
+ *         description: Serviço de tarefa atualizado
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ *   delete:
+ *     tags:
+ *       - Tasks Services
+ *     summary: Deletar serviço de tarefa
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       200:
+ *         description: Serviço de tarefa deletado com sucesso
+ *       401:
+ *         $ref: '#/components/responses/UnauthorizedError'
+ */
+
 import { authenticate } from '@feathersjs/authentication'
 
 import { hooks as schemaHooks } from '@feathersjs/schema'
